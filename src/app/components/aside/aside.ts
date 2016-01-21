74d465388afd11fc6c00a37972be0f73f27daccd
marcincichocki <marcincichocki1993@gmail.com>
2016-01-21 15:41:21 +0100

    FEAT: add search component

--- src/app/components/aside/aside.ts
+++ src/app/components/aside/aside.ts
@@ -1,8 +1,11 @@
 import {Component} from 'angular2/core';
 
+import {SearchComponent} from '../search/search';
+
 
 @Component({
   selector: '[aside-component]',
-  template: require('./aside.html')
+  template: require('./aside.html'),
+  directives: [SearchComponent]
 })
 export class AsideComponent {}
---
+++ src/app/components/search/search.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'search-component',
+  template: require('./search.html')
+})
+export class SearchComponent {}
