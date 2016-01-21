a2acaf63ef5b6bc76031e513fc319db92ff207b3
marcincichocki <marcincichocki1993@gmail.com>
2016-01-21 15:32:18 +0100

    FEAT: add basic implmentation of aside

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -1,8 +1,11 @@
 import {Component} from 'angular2/core';
 
+import {AsideComponent} from '../aside/aside';
+
 
 @Component({
   selector: 'app-component',
   template: require('./app.html'),
+  directives: [AsideComponent]
 })
 export class AppComponent {}
