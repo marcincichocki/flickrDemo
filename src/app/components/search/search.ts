81f304234ff11f2e3c0aee1dd60f2421a9f1be88
marcincichocki <marcincichocki1993@gmail.com>
2016-01-21 15:48:49 +0100

    FEAT: add simple form

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,8 +1,26 @@
 import {Component} from 'angular2/core';
+import {NgModel} from 'angular2/common';
 
 
 @Component({
   selector: 'search-component',
-  template: require('./search.html')
+  template: require('./search.html'),
+  directives: [NgModel]
 })
-export class SearchComponent {}
+export class SearchComponent {
+
+  // Store user's input.
+  private model = {
+    query: null
+  };
+
+
+  /**
+   * Makes request to the endpoint for given params.
+   */
+  onSubmit(): void {
+
+    // make request here
+  }
+
+}
